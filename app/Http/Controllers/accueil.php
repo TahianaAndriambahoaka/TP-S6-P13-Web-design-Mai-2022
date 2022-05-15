89d5abb69aaf3797282541652ea458ae06dee90c
Tahiana Andriambahoaka <89937368+TahianaAndriambahoaka@users.noreply.github.com>
2022-05-15 15:11:34 +0300

    initial commit

---
+++ app/Http/Controllers/accueil.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Image;
+
+class accueil extends Controller
+{
+    public function index() {
+        Cache::forever('cle', 'voici la valeur du cache');
+        // Cache::flush();
+        dd(
+            Cache::get('cle')
+        );
+        return view('welcome');
+    }
+    // /**
+    //  * Show the form for creating a new resource.
+    //  *
+    //  * @return \Illuminate\Http\Response
+    //  */
+    public function resizeImage()
+    {
+        return view('welcome');
+    }
+  
+    // /**
+    //  * Show the form for creating a new resource.
+    //  *
+    //  * @return \Illuminate\Http\Response
+    //  */
+    public function resizeImagePost(Request $request)
+    {
+        $this->validate($request, [
+            'title' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:20480',
+        ]);
+  
+        $image = $request->file('image');
+        $input['imagename'] = time().'_'.$image->getClientOriginalName();
+     
+        $destinationPath = public_path('thumbnail');
+        $img = Image::make($image->getRealPath());
+        $img->resize(100, 100, function ($constraint) {
+            $constraint->aspectRatio();
+        })->save($destinationPath.'/'.$input['imagename']);
+   
+        $destinationPath = public_path('images');
+        $image->move($destinationPath, $input['imagename']);
+   
+        // $this->postImage->add($input);
+   
+        return back()
+            ->with('success','Image Upload successful')
+            ->with('imageName',$input['imagename']);
+    }
+}
